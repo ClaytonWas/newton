ba3df6dc1d93b82824c92051d78680d1067cc377
ClaytonWas <claywasmund@gmail.com>
2025-01-16 13:38:56 -0500

    Working enemy area detection and collision.

--- scripts/enemies/demon.gd
+++ scripts/enemies/demon.gd
@@ -1,14 +1,31 @@
-extends Node3D
+extends CharacterBody3D
 
-var currentFrame = Vector2i.ZERO
+@onready var sprite = $AnimatedSprite3D
+@onready var detectionArea = $DetectionArea
+var currentXSpeed = 5
+var xBasisMovementVector = Vector3(currentXSpeed, 0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var sprite3D = $Sprite3D
-	sprite3D.set_vframes(1)
-	sprite3D.set_hframes(4)
-	sprite3D.set_frame_coords(currentFrame)
+	sprite.play("walkForward")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:	
+	if position.x > 15:
+		xBasisMovementVector.x = -currentXSpeed
+	elif position.x < 0:
+		xBasisMovementVector.x = currentXSpeed
+
+# Called every physics frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(xBasisMovementVector*delta)
+	
+	if detectionArea.has_overlapping_bodies() == true:
+		var overlappingBodies = detectionArea.get_overlapping_bodies()
+		for body in overlappingBodies:
+			if body.name == 'Player':
+				sprite.billboard = 2
+			else:
+				sprite.billboard = 0
+	if collision:
+		print(collision.get_collider().name)
