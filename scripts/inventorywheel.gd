15ecd602e1467461c93983285b584621a23f4c15
ClaytonWas <claywasmund@gmail.com>
2025-01-19 17:35:44 -0500

    Added menu open check, currently just makes mouse visible.

--- scripts/inventorywheel.gd
+++ scripts/inventorywheel.gd
@@ -1,4 +1,3 @@
-@tool
 extends Control
 
 @export var bg_clr : Color
@@ -10,6 +9,7 @@ extends Control
 
 @export var options = []
 var inventoryOpen : bool = false
+var menuOpen: bool = false
 
 func _draw():
 	var points = []
@@ -21,7 +21,7 @@ func _draw():
 			for i in range(len(options) - 1):
 				var rads = TAU * i/((len(options) - 1))
 				points.append(Vector2.from_angle(rads))			
-									
+				
 				draw_line(points[-1]*inner_radius,points[-1]*outer_radius,Color.BLACK,border_width+4,true)	# Draws divider + border lines
 				draw_line(points[-1]*inner_radius,points[-1]*outer_radius,border_clr,border_width,true)
 				draw_arc(Vector2.ZERO, outer_radius, rads,  2*rads, 128, Color.BLACK)
@@ -30,18 +30,30 @@ func _draw():
 	
 func _ready():
 	pass
-	
-func _process(delta):
-	if(inventoryOpen):
-		self.visible = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	else:
-		self.visible = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta: float):
+	pass
+
+func _process(delta):
 	if Input.is_action_pressed("inventory_toggle"):
 		inventoryOpen = true
 	else:
 		inventoryOpen = false
-		
+
+	if(!menuOpen):
+		if inventoryOpen:
+			self.visible = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			self.visible = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		menuOpen = !menuOpen
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			inventoryOpen = false
+			self.visible = false
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
