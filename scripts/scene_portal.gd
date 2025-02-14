b54f024556f5c40a27a8a6f54b4f944e2f1a0954
ClaytonWas <claywasmund@gmail.com>
2025-02-14 14:24:59 -0500

    working portals for all scenes

--- scripts/scene_portal.gd
+++ scripts/scene_portal.gd
@@ -1,10 +1,10 @@
 extends Area3D
 
-@export var target_scene: PackedScene  # Drag and drop the target scene in the Inspector
+@export var scene_path: String = "res://scenes/levels/outdoors.tscn"
 
 func _ready():
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
-	if body.name == "Player" and target_scene:
-		get_tree().change_scene_to_packed(target_scene)  # Load the new scene
+	if body.name == "Player" and scene_path:
+		get_tree().change_scene_to_file(scene_path)
