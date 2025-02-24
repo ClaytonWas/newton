824154da5ada3f8595bb5fcf9b7c050478cbc90b
Andrew Cleeg <cleggster@hotmail.com>
2025-02-23 19:44:31 -0500

    animation tree

---
+++ scripts/enemies/mutant.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@onready var anim_tree = self.find_child('AnimationTree')
+@onready var anim_state = anim_tree.get("parameters/playback")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Boss has loaded")
+	anim_tree.active = true
+	
+	anim_state.travel("mutant_roaring")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
