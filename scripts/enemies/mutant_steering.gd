da9217fa983488ca6f570498d1f33736fae22696
Andrew Cleeg <cleggster@hotmail.com>
2025-03-31 11:16:57 -0400

    Boss health UI

--- scripts/enemies/mutant_steering.gd
+++ scripts/enemies/mutant_steering.gd
@@ -1,5 +1,13 @@
 extends CharacterBody3D
 
+enum State {
+	IDLE,
+	ALERTED,
+	CHASING,
+	ATTACKING,
+	STUNNED
+}
+
 @export var target_node: CharacterBody3D
 
 @export_category("Beginning State")
@@ -20,12 +28,14 @@ extends CharacterBody3D
 @export var hit_sound: AudioStreamWAV
 @export var death_sound = preload('res://sounds/Enemy/MutantBoss/mutant_die_sound.mp3')
 
+# Steering Addons
 @onready var agent := await GSAICharacterBody3DAgent.new(self)
 @onready var target := GSAIAgentLocation.new()
 @onready var accel := GSAITargetAcceleration.new()
 @onready var blend := GSAIBlend.new(agent)
 @onready var face := GSAIFace.new(agent, target, true)
 @onready var arrive := GSAIArrive.new(agent, target)
+# State Control Elements
 @onready var animation = $AnimationPlayer
 @onready var alert_timer = $AlertTimer
 @onready var attack_interval_timer = $AttackIntervalTimer
@@ -38,67 +48,7 @@ var aware_of_player: bool = false
 var can_attack: bool = true
 var stunned: bool = false
 var attack_counter: int	# Counter to alternate swipe and punch
-
-func _ready():
-	#anim_state.travel('mutant_idle')
-	animation.play('mutant_breathing_idle')
-	attack_counter = 0
-	
-func _physics_process(delta: float) -> void:
-
-		
-	match state:
-		State.CHASING:
-			if target_node:
-				target.position = target_node.transform.origin
-				target.position.y = transform.origin.y
-				blend.calculate_steering(accel)
-				agent._apply_steering(accel, delta)
-				navigation_agent.set_target_position(target_node.position)
-				var next_path_pos = navigation_agent.get_next_path_position()
-				var direction = (next_path_pos - global_position).normalized()
-				#Move based on steering addon
-				velocity = direction * movement_speed
-				move_and_slide()
-				if not audio.is_playing():
-					audio.stream = run_sound
-					audio.play()
-				if not (animation.current_animation == 'mutant_swiping' or animation.current_animation == 'mutant_roar' or animation.current_animation == 'mutant_punch'):
-					animation.play('mutant_run_(1)')
-				
-		State.ATTACKING:
-			# Decelleration when reaching player
-			velocity = Vector3.ZERO
-			if can_attack: #and bullet:
-				can_attack = false
-				attack_counter += 1
-				
-				if attack_counter % 2 == 0:
-					animation.play("mutant_swiping")
-					attack_interval_timer.wait_time = animation.get_animation("mutant_swiping").length
-				else:
-					animation.play("mutant_punch")
-					attack_interval_timer.wait_time = animation.get_animation("mutant_punch").length
-				attack_interval_timer.start()
-			
-		State.STUNNED:
-			print('Monster shot')
-			velocity = velocity.move_toward(Vector3.ZERO, movement_speed * delta)
-			move_and_slide()
-			animation.play("mutant_stun")
-			if not audio.is_playing():
-				audio.stream = hurt_sound
-		State.ALERTED:
-					
-			animation.play('mutant_roaring')
-		
-		State.IDLE:
-			animation.play('mutant_breathing_idle')
-			velocity = Vector3.ZERO
-			# Play sound	
-		_:
-			velocity = Vector3.ZERO
-
+var hit_counter: int	# Tracks how many times boss shot before stun
 
 func setup(
 	align_tolerance: float,
@@ -128,15 +78,69 @@ func setup(
 	self.target.position = target_node.transform.origin
 	blend.add(arrive, 1)
 	blend.add(face, 1)
-	print('Monster is setup for ',target_node.name)
+	print('Mutant is setup for ',target_node.name)
 
-enum State {
-	IDLE,
-	ALERTED,
-	CHASING,
-	ATTACKING,
-	STUNNED
-}
+func _ready():
+	animation.play('mutant_breathing_idle')
+	attack_counter = 0
+	hit_counter = 0
+
+	%HealthBar.max_value = %HealthComponent.max_health
+	%HealthBar.value = %HealthComponent.max_health
+	
+func _physics_process(delta: float) -> void:
+	match state:
+		State.CHASING:
+			if target_node and not stunned:
+				target.position = target_node.transform.origin
+				target.position.y = transform.origin.y
+				blend.calculate_steering(accel)
+				agent._apply_steering(accel, delta)
+				navigation_agent.set_target_position(target_node.position)
+				var next_path_pos = navigation_agent.get_next_path_position()
+				var direction = (next_path_pos - global_position).normalized()
+				#Move based on steering addon
+				velocity = direction * movement_speed
+				move_and_slide()
+				
+				if not audio.is_playing(): # Play run sound
+					audio.stream = run_sound
+					audio.play()
+				if not (animation.current_animation == 'mutant_swiping' or animation.current_animation == 'mutant_roar' or animation.current_animation == 'mutant_punch'):
+					animation.play('mutant_run_(1)')
+				
+		State.ATTACKING:
+			# Decelleration when reaching player
+			velocity = Vector3.ZERO
+			if can_attack: #and bullet:
+				can_attack = false
+				attack_counter += 1
+				
+				if not animation.current_animation == "mutant_stun":
+					if attack_counter % 2 == 0:
+						animation.play("mutant_swiping")
+						attack_interval_timer.wait_time = animation.get_animation("mutant_swiping").length
+					else:
+						animation.play("mutant_punch")
+						attack_interval_timer.wait_time = animation.get_animation("mutant_punch").length
+					attack_interval_timer.start()
+			
+		State.STUNNED:
+			velocity = velocity.move_toward(Vector3.ZERO, movement_speed * delta)
+			move_and_slide()
+			animation.play("mutant_stun")
+			if not audio.is_playing():
+				audio.stream = hurt_sound
+				
+		State.ALERTED:
+			animation.play('mutant_roaring')
+		
+		State.IDLE:
+			animation.play('mutant_breathing_idle')
+			velocity = Vector3.ZERO
+			# Play sound	
+		_:
+			velocity = Vector3.ZERO
 
 func instance_bullet():
 	var projectile = bullet.instantiate()
@@ -150,15 +154,13 @@ func instance_bullet():
 func _process(delta):
 	match state:
 		State.STUNNED:
-			
 			if not stunned and target_node:
-				
 				state = State.CHASING
 
 func _on_punch_finished():
 	animation.play("mutant_punch")
 	print('punching')
-	animation.animation_finished.disconnect(_on_punch_finished)
+	#animation.animation_finished.disconnect(_on_punch_finished)
 
 func _on_vision_area_body_entered(body):
 	if body.name == 'Player':
@@ -197,11 +199,14 @@ func _on_attack_interval_timer_timeout():
 	can_attack = true
 
 func _on_health_component_damage_taken():
-	state = State.STUNNED
-	stunned = true
-	stun_timer.start()
-	print('stunned')
-	# Play sound TODO
+	# When Boss is shot
+	%HealthBar.value = %HealthComponent.health
+	hit_counter += 1
+	if not stunned and hit_counter % 4 == 0:
+		state = State.STUNNED
+		stunned = true
+		stun_timer.start()
+		print('stunned')
 
 func _on_stun_timer_timeout():
 	stunned = false
